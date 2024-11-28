4c004a71e5aab78fda948723eb2c759859b2a76f
bibekbhatta <bibekbhatta@gmail.com>
2024-11-28 18:32:36 +0000

    Docstring streamlined

--- inflammation/models.py
+++ inflammation/models.py
@@ -1,19 +1,24 @@
-"""Module containing models representing patients and their data.
+"""
+
+Module containing models representing patients and their data.
 
 The Model layer is responsible for the 'business logic' part of the software.
 
 Patients' data is held in an inflammation table (2D array) where each row contains 
 inflammation data for a single patient taken over a number of days 
 and each column represents a single day across all patients.
+
 """
 
 import numpy as np
 
 
 def load_csv(filename):  
-    """Load a Numpy array from a CSV
+    """
+    Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
+
     """
     return np.loadtxt(fname=filename, delimiter=',')
 
@@ -29,6 +34,8 @@ def daily_max(data):
 
 
 def daily_min(data):
-    """Calculate the daily minimum of a 2D inflammation data array."""
+    """
+    Calculate the daily minimum of a 2D inflammation data array.
+    """
     return np.min(data, axis=0)
 
